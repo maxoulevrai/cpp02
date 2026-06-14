21c03c73bda08a9791862aada6a0b3afeaef02a8
maleca <maleca@student.42.fr>
2026-06-15 01:32:17 +0200

    ex00 probleme op affectation

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maleca <maleca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 22:02:07 by maleca            #+#    #+#             */
+/*   Updated: 2026/06/15 01:29:05 by maleca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_fractionnalBits = 8;
+
+Fixed::Fixed() : _rawBits(0) {
+	std::cout << "Default contructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "copy assignement operator called" << std::endl;
+	if (this != &other)
+		this->_rawBits = other.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+const int		Fixed::getRawBits() {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_rawBits);
+}
+
+void	Fixed::setRawBits(const int raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_rawBits = raw;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maleca <maleca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 22:41:48 by maleca            #+#    #+#             */
+/*   Updated: 2026/06/14 22:50:14 by maleca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void) {
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
